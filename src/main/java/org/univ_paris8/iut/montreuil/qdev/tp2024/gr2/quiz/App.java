2d7c5169e9ba27481d87a437d3d759eacd535f7c
ybenyoussef <yasmine.benyoussef01@gmail.com>
2024-05-07 11:37:48 +0200

    init project

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/App.java
@@ -0,0 +1,13 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/AppTest.java
@@ -0,0 +1,21 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
