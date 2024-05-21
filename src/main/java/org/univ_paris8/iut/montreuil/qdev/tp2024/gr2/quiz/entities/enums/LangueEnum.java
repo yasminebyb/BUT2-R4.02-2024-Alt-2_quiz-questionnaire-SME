ebac445c149270c70fb891df65a40074f8c609f4
MarielleVallee <marielle.vallee@gmail.com>
2024-05-21 16:42:57 +0200

    Revue énumérations

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/entities/enums/LangueEnum.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/entities/enums/LangueEnum.java
@@ -2,6 +2,16 @@ package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.entities.enums;
 
 public enum LangueEnum {
 
-    français,english,espana,deutsh,italia;
+    français("fr"),english("en"),espana("es"),deutsh("de"),italia("it");
+
+    private final String valeur;
+
+    private LangueEnum(String valeur) {
+        this.valeur = valeur;
+    }
+
+    public String getValeur() {
+        return this.valeur;
+    }
 
 }
--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/entities/enums/NiveauDifficulteQuestion.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/entities/enums/NiveauDifficulteQuestion.java
@@ -1,5 +1,14 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.quiz.entities.enums;
 
 public enum NiveauDifficulteQuestion {
-    Simple,intermedaire,expert;
+    Simple(1),intermedaire(2),expert(3);
+    private final int valeur;
+
+    private NiveauDifficulteQuestion(int valeur) {
+        this.valeur = valeur;
+    }
+
+    public int getValeur() {
+        return this.valeur;
+    }
 }
