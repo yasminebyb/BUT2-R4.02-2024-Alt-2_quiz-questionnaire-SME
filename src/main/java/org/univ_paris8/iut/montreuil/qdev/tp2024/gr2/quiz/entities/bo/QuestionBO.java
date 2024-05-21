7fb8a8fa7a8480956c17377f1ba9180238a1b46a
MarielleVallee <marielle.vallee@gmail.com>
2024-05-21 17:38:24 +0200

    attribut langue ajout

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/entities/bo/QuestionBO.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/quiz/entities/bo/QuestionBO.java
@@ -4,14 +4,17 @@ public class QuestionBO {
 
     private int id;
     private int num;
+    private String langue;
     private String question;
     private String reponse;
+
+
     private int difficulte;
     private String explication;
     private String reference;
 
 
-    public QuestionBO(int id, int num, String question, String reponse, int difficulte, String explication, String reference) {
+    public QuestionBO(int id, int num, String question, String reponse, int difficulte, String explication, String reference, String langue) {
         this.id = id;
         this.num = num;
         this.question = question;
@@ -19,6 +22,7 @@ public class QuestionBO {
         this.difficulte = difficulte;
         this.explication = explication;
         this.reference = reference;
+        this.langue = langue;
     }
 
     public int getId() {
